5311ee83cf0f6c5f1dd99b01455ef9e6d271227b
Michael Schou <mschou@gmail.com>
2016-06-30 16:33:15 -0700

    add in get/post

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ def main():
 
 @app.route('/index',methods= ['GET','POST'])
 def index():
-  return render_template('index.html')
+  	#return render_template('index.html')
+  	if request.method=='GET':
+  		return render_template('index.html')
+		#return render_template('userinfo_schou.html')
+	else:
+		return render_template('index.html')
+		#app_schou.vars['stock'] = request.form['stock_schou']
+		#return redirect('/main')	
 
 if __name__ == '__main__':
   app.run(port=33507)
